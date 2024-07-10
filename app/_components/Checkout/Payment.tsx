2503a63be6c9e22d585c6f76e0b3eea17b95ae9a
Isaac Awonugba <it.awonugba@gmail.com>
2024-07-10 09:37:02 +0100

    Initial Checkout

--- app/_components/Checkout/Payment.tsx
+++ app/_components/Checkout/Payment.tsx
@@ -1,3 +1,15 @@
+import FeaturedMenu from "../Menu/FeaturedMenu";
+import Recommendation from "../Recommendation/Recommendation";
+
 export default function Payment() {
-  return <section></section>;
+  return (
+    <>
+      <section>
+        <FeaturedMenu />
+      </section>
+      <div className="mt-2 mb-16">
+        <Recommendation />
+      </div>
+    </>
+  );
 }
