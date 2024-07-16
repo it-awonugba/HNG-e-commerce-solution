a59c1eefad50623c0fa75e34928002ac0e717d9f
WebKing <isaac@afrocenchix.com>
2024-07-16 17:17:31 +0100

    bug fix

---
+++ app/_components/Product/Singleton.tsx
@@ -0,0 +1,58 @@
+"use client";
+import Image from "next/image";
+
+import { useFetchProducts } from "@/app/_hooks/useFetchData";
+import { useFormatImage } from "@/app/_hooks/useFormatImage";
+import { useFormatPrice } from "@/app/_hooks/useFormatPrice";
+import { useCart } from "@/app/_context/CartContext";
+import { Button } from "@/components/ui/button";
+
+export type SingletonProps = {
+  id: string;
+};
+
+export default function Singleton({ id }: SingletonProps) {
+  const { products, loading, error } = useFetchProducts();
+  const { formatImage } = useFormatImage();
+  const { formatPrice } = useFormatPrice();
+  const { addProductToCart } = useCart();
+
+  const product = products.find((item) => item.id == id);
+
+  if (loading || !product || !id) {
+    return <div>...loading</div>;
+  }
+
+  return (
+    <section className="flex flex-col px-6 py-4 lg:px-[7.5rem] lg:py-8 md:flex-row p-4 gap-4">
+      <div className="w-full md:w-1/2">
+        <Image
+          src={formatImage(product)}
+          alt={product.name}
+          width={200}
+          height={160}
+          priority={true}
+          className="w-full h-auto"
+        />
+      </div>
+      <div className="w-full md:w-1/2">
+        <h1 className="text-2xl font-bold mb-4">{product.name}</h1>
+        <p className="text-lg font-semibold">{formatPrice(product)}</p>
+        <p className="mb-4">{product.description}</p>
+
+        <Button
+          onClick={(e) => {
+            e.preventDefault();
+            addProductToCart(product);
+            window.scrollTo({
+              top: 0,
+              behavior: "smooth",
+            });
+          }}
+        >
+          Add to Cart
+        </Button>
+      </div>
+    </section>
+  );
+}
--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,61 +1,18 @@
 "use client";
-
-import Image from "next/image";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useFetchProducts } from "@/app/_hooks/useFetchData";
-import { useFormatImage } from "@/app/_hooks/useFormatImage";
-import { useFormatPrice } from "../_hooks/useFormatPrice";
-import { useCart } from "../_context/CartContext";
-import { Button } from "@/components/ui/button";
-
-export type ProductPageContentProps = {
-  id: string | null;
-};
+import { Suspense } from "react";
+import { useSearchParams } from "next/navigation";
+import Singleton from "../_components/Product/Singleton";
 
 export default function Page() {
-  const router = useRouter();
-  const { products, loading, error } = useFetchProducts();
-  const { formatImage } = useFormatImage();
-  const { formatPrice } = useFormatPrice();
-  const { addProductToCart } = useCart();
   const searchParam = useSearchParams();
   const id = searchParam?.get("id");
-
-  const product = products.find((item) => item.id == id);
-
-  if (loading || !product || !id) {
+  if (!id) {
     return <div>...loading</div>;
   }
 
   return (
-    <section className="flex flex-col px-6 py-4 lg:px-[7.5rem] lg:py-8 md:flex-row p-4 gap-4">
-      <div className="w-full md:w-1/2">
-        <Image
-          src={formatImage(product)}
-          alt={product.name}
-          width={200}
-          height={160}
-          className="w-full h-auto"
-        />
-      </div>
-      <div className="w-full md:w-1/2">
-        <h1 className="text-2xl font-bold mb-4">{product.name}</h1>
-        <p className="text-lg font-semibold">{formatPrice(product)}</p>
-        <p className="mb-4">{product.description}</p>
-
-        <Button
-          onClick={(e) => {
-            e.preventDefault();
-            addProductToCart(product);
-            window.scrollTo({
-              top: 0,
-              behavior: "smooth",
-            });
-          }}
-        >
-          Add to Cart
-        </Button>
-      </div>
-    </section>
+    <Suspense fallback={null}>
+      <Singleton id={id} />
+    </Suspense>
   );
 }
